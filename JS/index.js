0720cabf548dddaa045eed352e2ff37d43047e0c
Juan Ignacio Tejada <tejadajuanignacio12@gmail.com>
2020-09-16 20:09:09 -0300

    New style files

---
+++ JS/index.js
@@ -0,0 +1,22 @@
+
+//Logica del boton para subir al top
+const mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+//
+
